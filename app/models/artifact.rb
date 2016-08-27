8d8ce8bb94e341f57a95f03974f4f5b04b4f96d0
Adam Lesniak <adam@lesniak.pl>
2016-08-27 23:18:37 +0100

    Add upload_to_s3 to Artifact methods

--- app/models/artifact.rb
+++ app/models/artifact.rb
@@ -1,6 +1,9 @@
 class Artifact < ActiveRecord::Base
+  
+  before_save :upload_to_s3
   attr_accessor :upload
   belongs_to :project
+  
   
   MAX_FILESIZE = 10.megabytes
   validates_presence_of   :name, :upload
@@ -10,6 +13,14 @@ class Artifact < ActiveRecord::Base
   validate :uploaded_file_size
   
   private
+  
+  def upload_to_s3
+    s3 = AWS::S3::Resource.new
+    tenant_name = Tenant.find(Thread.current[:tenant_id]).name
+    obj = s3.bucket(ENV['AWS_S3_BUCKET']).object("#{tenant_name}/#{upload.original_filename}")
+    obj.upload_file(upload.path, acl: 'public-read')
+    self.key = obj.public_url
+  end
   
   def uploaded_file_size
     if upload
